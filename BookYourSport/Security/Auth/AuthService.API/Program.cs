a3d8564c84a939da8927a140cebe2e27a642e12d
Milica19105 <milicatosic105@gmail.com>
2026-08-12 22:14:04 +0200

    feat: add user entity/data model

--- BookYourSport/Security/Auth/AuthService.API/Program.cs
+++ BookYourSport/Security/Auth/AuthService.API/Program.cs
@@ -1,16 +1,20 @@
+using AuthService.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 
 app.Run();
