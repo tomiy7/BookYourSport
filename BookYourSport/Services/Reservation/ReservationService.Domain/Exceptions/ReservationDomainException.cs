467340fc4acc3faecb41fc9370ea68ffa56565f6
tomiy7 <radenko.nikolic01@gmail.com>
2026-08-13 00:19:26 +0200

    fix(reservation): didn't add domain exception in last commit (#6)

---
+++ BookYourSport/Services/Reservation/ReservationService.Domain/Exceptions/ReservationDomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace ReservationService.Domain.Exceptions;
+
+public class ReservationDomainException : Exception
+{
+    public ReservationDomainException() { }
+    
+    public ReservationDomainException(string message) : base(message) { }
+    
+    public ReservationDomainException(string message, Exception innerException) : base(message, innerException) { }
+}
