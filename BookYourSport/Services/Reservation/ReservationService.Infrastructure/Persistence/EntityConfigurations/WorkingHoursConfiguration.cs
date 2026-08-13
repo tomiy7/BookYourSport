25c98134311795061116e333b4f6e6a525567bc5
tomiy7 <radenko.nikolic01@gmail.com>
2026-08-14 00:23:26 +0200

    feat(reservation): add WorkingHours EF Core entity configuration (#32)

---
+++ BookYourSport/Services/Reservation/ReservationService.Infrastructure/Persistence/EntityConfigurations/WorkingHoursConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReservationService.Domain.Entities;
+
+namespace ReservationService.Infrastructure.Persistence.EntityConfigurations;
+
+public class WorkingHoursConfiguration : IEntityTypeConfiguration<WorkingHours>
+{
+    public void Configure(EntityTypeBuilder<WorkingHours> builder)
+    {
+        builder.ToTable("working_hours");
+        builder.HasKey(x => x.Id);
+        builder.Property(w => w.DayOfWeek).HasConversion<string>().HasMaxLength(15);
+        
+        builder.HasIndex(w => new { w.ClubId, w.DayOfWeek }).IsUnique();
+    }
+}
