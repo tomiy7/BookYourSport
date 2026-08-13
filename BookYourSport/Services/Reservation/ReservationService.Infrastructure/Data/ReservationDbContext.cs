00d067e77ee668efe226cbf7714a84ab8d9fc32d
tomiy7 <radenko.nikolic01@gmail.com>
2026-08-14 01:25:21 +0200

    feat(reservation): implement ReservationDbContext with owned types and audit fields (#33)

---
+++ BookYourSport/Services/Reservation/ReservationService.Infrastructure/Data/ReservationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReservationService.Domain.Common;
+using ReservationService.Domain.Entities;
+using ReservationService.Infrastructure.Persistence.EntityConfigurations;
+
+namespace ReservationService.Infrastructure.Data;
+
+public class ReservationDbContext : DbContext
+{
+    public ReservationDbContext(DbContextOptions<ReservationDbContext> options)
+        : base(options) { }
+    
+    public DbSet<TennisClub> TennisClubs => Set<TennisClub>();
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<Entity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedDate = DateTime.UtcNow;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                    break;
+            }
+        }
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TennisClubConfiguration());
+        modelBuilder.ApplyConfiguration(new CourtConfiguration());
+        modelBuilder.ApplyConfiguration(new WorkingHoursConfiguration());
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
