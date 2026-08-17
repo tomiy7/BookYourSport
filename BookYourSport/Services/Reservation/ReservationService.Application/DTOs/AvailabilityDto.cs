9c67726a6eb253f3f748995a527295b166e62b6e
tomiy7 <radenko.nikolic01@gmail.com>
2026-08-17 23:33:49 +0200

    feat(reservation): add availability and reservation DTOs (#18)

---
+++ BookYourSport/Services/Reservation/ReservationService.Application/DTOs/AvailabilityDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ReservationService.Application.DTOs;
+
+public class AvailabilityDto
+{
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
---
+++ BookYourSport/Services/Reservation/ReservationService.Application/DTOs/ReservationDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationService.Application.DTOs;
+
+public class ReservationDto
+{
+    public Guid Id { get; set; }
+    public Guid CourtId { get; set; }
+    public Guid ClubId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public PriceDto Price { get; set; } = new();
+    public string Status { get; set; } = string.Empty;
+}
+
+public class CreateReservationDto
+{
+    [Required]
+    public Guid UserId { get; set; }
+    
+    [Required]
+    public DateTime StartTime { get; set; }
+    
+    [Required]
+    public DateTime EndTime { get; set; }
+}
+
+public class RescueReservationDto
+{
+    [Required]
+    public DateTime NewStartTime { get; set; }
+    
+    [Required]
+    public DateTime NewEndTime { get; set; }
+}
